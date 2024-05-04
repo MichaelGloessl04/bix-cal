40de62ff02d8976b4510960481f09cafe1aa02f1
MichaelGloessl04 <gloess190117@gmail.com>
2024-05-04 14:41:04 +0200

    awwa

--- backend/main.py
+++ backend/main.py
@@ -32,7 +32,7 @@ app.add_middleware(
 )
 
 
-@app.get('/person', response_model=List[ApiTypes.Person])
+@app.get('/person/', response_model=List[ApiTypes.Person])
 async def get_persons():
     return resources['crud'].get(Models.Person)
 
@@ -47,6 +47,31 @@ async def get_person_entries(person_id: int):
     return resources['crud'].search(Models.Entry, ['person_id'], str(person_id))
 
 
-@app.post('/person', response_model=ApiTypes.Person)
+@app.post('/person/', response_model=ApiTypes.Person)
 async def create_person(person: ApiTypes.Person):
     return resources['crud'].create(Models.Person, person.dict())
+
+
+@app.put('/person/{person_id}', response_model=ApiTypes.Person)
+async def update_person(person_id: int, person: ApiTypes.Person):
+    return resources['crud'].update(Models.Person, person_id, person.dict())
+
+
+@app.get('/entry/', response_model=List[ApiTypes.Entry])
+async def get_entries():
+    return resources['crud'].get(Models.Entry)
+
+
+@app.get('/entry/{entry_id}', response_model=ApiTypes.Entry)
+async def get_entry(entry_id: int):
+    return resources['crud'].get_single(Models.Entry, entry_id)
+
+
+@app.post('/entry/', response_model=ApiTypes.Entry)
+async def create_entry(entry: ApiTypes.Entry):
+    return resources['crud'].create(Models.Entry, entry.dict())
+
+
+@app.put('/entry/{entry_id}', response_model=ApiTypes.Entry)
+async def update_entry(entry_id: int, entry: ApiTypes.Entry):
+    return resources['crud'].update(Models.Entry, entry_id, entry.dict())
