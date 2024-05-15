3f0f009a654ca617ccb9c8528fcd74fa1e91b6ee
MichaelGloessl04 <gloess190117@gmail.com>
2024-05-15 16:38:58 +0200

    wowowowo

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,14 +3,22 @@ import 'bootstrap'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import { createApp } from 'vue'
+import { VueFire, VueFireAuth } from 'vuefire'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import { firebaseApp } from '@/firebase'
 
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
 app.use(pinia)
+app.use(VueFire, {
+    firebaseApp,
+    modules: [
+      VueFireAuth(),
+    ],
+  })
 
 app.mount('#app')
--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,68 +1,34 @@
-<template>
-    <div class="hover-box">
-        <h1>Login</h1>
-        <form @submit.prevent="login">
-            <div class="form-group">
-                <label for="username">Username</label>
-                <input type="text" class="form-control" id="username" v-model="username">
-            </div>
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" class="form-control" id="password" v-model="password">
-            </div>
-            <div class="form-check">
-                <input v-model="remember" type="checkbox" class="form-check-input" id="remember">
-                <label class="form-check-label" for="remember">Remember me</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Login</button>
-        </form>
-    </div>
-</template>
+<script setup>
+import {
+  getRedirectResult,
+  signInWithRedirect,
+  signOut,
+} from 'firebase/auth'
+import { useCurrentUser, useFirebaseAuth } from 'vuefire'
 
-<script setup lang="ts">
-import { ref } from 'vue'
-import { useRouter } from 'vue-router';
+const auth = useFirebaseAuth() // only exists on client side
 
-import { useUserStore } from '@/stores/user'
-import { getUser } from '@/scripts/api_calls';
-
-const username = ref('')
-const password = ref('')
-const remember = ref(false)
-
-const router = useRouter()
-const userStore = useUserStore()
-
-async function login() {
-    console.log('Login clicked')
-    getUser(username.value, password.value)
-        .then((user) => {
-            userStore.setUser(user)
-            router.push('/')
-        })
-        .catch((error) => {
-            console.error(error)
-            alert(`Login failed: ${error.message}`)
-            return
-        })
+// display errors if any
+const error = ref(null)
+function signinRedirect() {
+  signInWithRedirect(auth, someAuthProvider).catch((reason) => {
+    console.error('Failed signinRedirect', reason)
+    error.value = reason
+  })
 }
+
+// only on client side
+onMounted(() => {
+  getRedirectResult(auth).catch((reason) => {
+    console.error('Failed redirect result', reason)
+    error.value = reason
+  })
+})
 </script>
 
-<style scoped>
-h1 {
-    font-size: 2rem;
-    margin-bottom: 1rem;
-}
-
-button {
-    margin-top: 1rem;
-}
-
-.hover-box {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    top: 40%;
-}
-</style>
+<template>
+  <main>
+    <ErrorBox v-if="error" :error="error" />
+    <button @click="signinRedirect()">SignIn with Google</button>
+  </main>
+</template>
